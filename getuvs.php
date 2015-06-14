a4d3b85c65a91256419cf41511781586942a6ff3
Samuel Favrichon <samfav@users.noreply.github.com>
2015-06-14 13:10:29 +0200

    add titre to json

--- getuvs.php
+++ getuvs.php
@@ -4,16 +4,17 @@ include('db_connect.php');
 $result = array();
 
 if (!empty($_POST['branche']))
-	$req = "SELECT b.uv AS uv, COUNT(d.id) AS count FROM uvbranche b LEFT OUTER JOIN docs d ON d.uv=b.uv
+	$req = "SELECT b.uv AS uv, b.titreuv as nom,  COUNT(d.id) AS count FROM uvbranche b LEFT OUTER JOIN docs d ON d.uv=b.uv
 			WHERE branche='".$_POST['branche']."'  GROUP BY b.uv ORDER BY b.uv;";
 else
-	$req = "SELECT b.uv AS uv, COUNT(d.id) AS count FROM uvbranche b LEFT OUTER JOIN docs d ON d.uv=b.uv GROUP BY uv ORDER BY uv;";
+	$req = "SELECT b.uv AS uv, b.titreuv as nom, COUNT(d.id) AS count FROM uvbranche b LEFT OUTER JOIN docs d ON d.uv=b.uv GROUP BY uv ORDER BY uv;";
 
 $retour = db_query($req);
 
 while (($row = mysql_fetch_array($retour)) != 0) {
 	$uv = array (
 		'uvname' => $row['uv'],
+		'uvtitre' => $row['nom'],
 		'nbdocs' => $row['count']
 		);
 
