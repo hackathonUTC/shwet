ae1987b26fba9f292c984cecd63b7ed25de0dc30
Mewen <mewenmichel@yahoo.fr>
2015-06-14 07:50:50 +0200

    fix bug getuvs

--- getuvs.php
+++ getuvs.php
@@ -7,7 +7,7 @@ if (!empty($_POST['branche']))
 	$req = "SELECT b.uv AS uv, COUNT(d.id) AS count FROM uvbranche b LEFT OUTER JOIN docs d ON d.uv=b.uv
 			WHERE branche='".$_POST['branche']."'  GROUP BY b.uv ORDER BY b.uv;";
 else
-	$req = "SELECT uv, COUNT(*) AS count FROM docs GROUP BY uv ORDER BY uv;";
+	$req = "SELECT b.uv AS uv, COUNT(d.id) AS count FROM uvbranche b LEFT OUTER JOIN docs d ON d.uv=b.uv GROUP BY uv ORDER BY uv;";
 
 $retour = db_query($req);
 
