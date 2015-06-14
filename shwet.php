c50b00f88553a87406d64bca8d6ecb21076ddb76
NicolasSzewe <NicolasSzewe@users.noreply.github.com>
2015-06-14 06:31:57 +0200

    Update shwet.php -> header

---
+++ shwet.php
@@ -0,0 +1,24 @@
+<?php include 'header.html';?>
+
+<div class="container">
+
+      <div class="row-fluid">
+        <div class="row-fluid">
+  <div class="span12">
+      <div class="hero-unit">
+        <h1>Bienvenue sur Shwet.</h1>
+        <p>Pour consulter les documents d'une UV ou pour donner les tiens, tape son nom dans la barre de recherche (par exemple, "MT23").</p>
+      </div>
+       <form id="search-uv-form" class="form-search navbar-search" method="GET" action="uv.php">
+              <div class="input-append">
+                <input style="width: 1000px;" name="u" autocomplete="on" id="search-uv-name" class="search-query span2" type="text" placeholder="Rechercher une UV">
+                <button type="submit" class="btn btn-primary">
+                  <img height="20" width="20" src="img/loupe.ico"></a>
+                </button>
+              </div>
+        </form>
+	</div>
+</div>
+
+  </body>
+</html>
