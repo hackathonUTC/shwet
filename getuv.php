6b8df34fe4ba1baaccdf0631d4893757a2374fa4
Mewen <mewenmichel@yahoo.fr>
2015-06-14 05:50:36 +0200

    Maj getuv

--- getuv.php
+++ getuv.php
@@ -3,21 +3,19 @@ include('db_connect.php');
 
 $result = array();
 
-$req = "SELECT * FROM uvbranche b, docs d WHERE d.uv=b.uv AND b.uv='".$_POST['uv']."';";
+$req = "SELECT * FROM uvbranche b WHERE b.uv='".$_POST['uv']."';";
 
 $retour = db_query($req);
 
-$row = mysql_fetch_array($retour);
-
-$result = array (
+while (($row = mysql_fetch_array($retour)) != 0) {
+	$uv = array (
 		'branche' => $row["branche"],
 		'uv' => $row["uv"],
-		'titreuv' => $row["titreuv"],
-		'id' => $row["id"],
-		'type' => $row["type"],
-		'nom' => $row["nom"],
-		'extension' => $row["extension"]
-	);
+		'titreuv' => $row["titreuv"]
+		);
+
+	array_push($result, $uv);
+}
 
 // var_dump($result);
 echo json_encode($result);
