d5c35bdf1afca8da231d78527a3471325829e126
NicolasSzewe <NicolasSzewe@users.noreply.github.com>
2015-06-14 06:55:05 +0200

    update verify.php

--- verify.php
+++ verify.php
@@ -7,9 +7,5 @@ if (!isset($_SESSION['user']))
 	$user = CAS::authenticate();
 	if ($user != -1) $_SESSION['user'] = $user;
 }
-if (isset($_SESSION['user']))
-{
-	require_once 'MAJ.class.php';
-}
 else CAS::login();
 ?>
