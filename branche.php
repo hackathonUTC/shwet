89d79e720ee2ec4243788234a2be095bc1d94f36
NicolasSzewe <NicolasSzewe@users.noreply.github.com>
2015-06-14 06:32:52 +0200

    update branche.php -> header

---
+++ branche.php
@@ -0,0 +1,37 @@
+<?php include 'header.html';?>
+
+
+<br><br>
+<div class="container">        
+<div class="row-fluid">
+	<div class="span12">
+		<ul class="nav nav-tabs">
+                  <li id="GBSelecteur"><a  href="branche.html?b=GB">GB</a></li>
+                  <li id="GISelecteur"><a  href="branche.html?b=GI">GI</a></li>
+                  <li id="GMSelecteur"><a  href="branche.html?b=GM">GM</a></li>
+                  <li id="GPSelecteur"><a  href="branche.html?b=GP">GP</a></li>
+                  <li id="GSMSelecteur"><a  href="branche.html?b=GSM">GSM</a></li>
+                  <li id="GSUSelecteur"><a  href="branche.html?b=GSU">GSU</a></li>
+                  <li id="TCSelecteur"><a  href="branche.html?b=TC">TC</a></li>
+                  <li id="TSHSelecteur"><a  href="branche.html?b=TSH">TSH</a></li>
+                  <li id="ALLSelecteur"><a  href="branche.html?b=all">Toutes</a></li>
+		</ul>
+	</div>
+</div>
+
+
+<div class="page-header text-center">
+	<div class="row-fluid">
+		<div class="offset2 span8">
+	<div id="title"><h1></h1></div>	
+	<h2> Toutes les UVs <small>Ordre Alphabétique</small></h2>
+		</div>
+	</div>
+</div>
+
+<div id="list-container" class="row-fluid">
+</div>
+</div>
+    </div>
+  </body>
+</html>
