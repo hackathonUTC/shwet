0fa4b4158f0ea154bafe0f58716d7f1dcead562b
Mewen <mewenmichel@yahoo.fr>
2015-06-14 11:32:37 +0200

    Oups ><

--- verify_connection.php
+++ verify_connection.php
@@ -4,7 +4,7 @@ require_once 'CONF.class.php';
 require_once 'CAS.class.php';
 $user = CAS::authenticate();
 if ($user == -1){
-	echo "pas connecté";
-	header('Location: shwet.php');
-	// CAS::login();
+	// echo "pas connecté";
+	// header('Location: shwet.php');
+	CAS::login();
 }
