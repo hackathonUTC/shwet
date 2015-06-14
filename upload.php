1b157bc958f8d9a979b76f9a34cfb6c86421282e
NicolasSzewe <NicolasSzewe@users.noreply.github.com>
2015-06-14 11:26:56 +0200

    Create upload.php

---
+++ upload.php
@@ -0,0 +1,20 @@
+<?php
+function upload($index,$destination)
+{
+   //Test1: fichier correctement uploadé
+     if (!isset($_FILES[$index]) OR $_FILES[$index]['error'] > 0) return FALSE;
+   //Déplacement
+     return move_uploaded_file($_FILES[$index]['tmp_name'],$destination);
+}
+  	$uv = $_POST['u'];  	
+	$type = $_POST['selectType'];
+  	$nom = $_POST['nomfichier'];
+  	$note = $_POST['note'];
+  	$semestre = $_POST['semestre'];
+  	$commentaire = $_POST['commentaire'];
+  	$filename = basename($_FILES['fichierUpload']['name']);
+	$ext = pathinfo($filename, PATHINFO_EXTENSION);  	
+	
+  	$upload_fichier = upload($filename,"docs"+$uv+"/"+$type+"/"+$id+"/"+$ext);
+
+  ?>
