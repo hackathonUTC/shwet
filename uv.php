8693ec470ff2f1b7f4d6e2e623b55619ad4e5ce8
Mewen <mewenmichel@yahoo.fr>
2015-06-14 10:40:24 +0200

    Up centrage vue

--- uv.php
+++ uv.php
@@ -2,16 +2,16 @@
 session_start();
 include 'header.php';
 ?>
+		<div class="container main-container">
+			<div class="row-fluid">
+				<div id="title">
+					<h1>
+					</h1>
+				</div>
 
-<div class="container main-container">
-
-	
-	<div id="title"><h1></h1></div>	
-
-</div>
-
-<div id="docs-container" class="row-fluid">
-</div>
-
-  </body>
+				<div id="docs-container" class="row-fluid">
+				</div>
+			</div>
+		</div>
+	</body>
 </html>
