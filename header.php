57224beaa3d4a40403258691b39f24f55ce28e0d
NicolasSzewe <NicolasSzewe@users.noreply.github.com>
2015-06-14 08:44:38 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,25 +1,5 @@
  <?php header('Access-Control-Allow-Origin: *'); ?>
 
-<?php
-session_save_path('sess');
-session_start();
-require_once 'CONF.class.php';
-require_once 'CAS.class.php';
-
-if (isset($_SESSION['user'])) require_once 'view/index.php';
-else
-{
-	$user = CAS::authenticate();
-	if ($user != -1)
-	{
-		$_SESSION['user'] = $user;
-		$_SESSION['ticket'] = $_GET['ticket'];
-		header('Location: ./');
-	}
-	else CAS::login();
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
