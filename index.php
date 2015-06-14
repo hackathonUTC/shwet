bdcdcada62eea5f86d9d2e2b8f300ced1e3b29a7
NicolasSzewe <NicolasSzewe@users.noreply.github.com>
2015-06-14 06:35:49 +0200

    update url location

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ else
 	{
 		$_SESSION['user'] = $user;
 		$_SESSION['ticket'] = $_GET['ticket'];
-		header('Location: shwet/shwet.php');
+		header('Location: shwet.php');
 	}
 	else CAS::login();
 }
