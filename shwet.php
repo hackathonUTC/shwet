22a8d403d862bb9850bbaa4991d9437819770799
NicolasSzewe <NicolasSzewe@users.noreply.github.com>
2015-06-14 08:42:54 +0200

    Update shwet.php

--- shwet.php
+++ shwet.php
@@ -7,7 +7,8 @@
   <div class="span12">
       <div class="hero-unit">
         <h1>Bienvenue sur le nouveau Shwet.</h1>
-        <p>Shwet IS BACK ! Pour consulter les documents d'une UV ou pour donner les tiens, tape son nom dans la barre de recherche (par exemple, "MT23").</p>
+        <p>Shwet IS BACK ! 
+        Pour consulter les documents d'une UV ou pour donner les tiens, tape son nom dans la barre de recherche (par exemple, "MT23").</p>
       </div>
        <form id="big-search-uv-form" class="form-search navbar-search" method="GET" action="uv.php">
               <div class="input-append">
