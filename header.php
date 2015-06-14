f52575ed2b5b7a656ac3ea80bde5488718c24797
NicolasSzewe <NicolasSzewe@users.noreply.github.com>
2015-06-14 06:56:04 +0200

    header.php

--- header.php
+++ header.php
@@ -76,6 +76,18 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 </ul>
               </li>
               </li>
+              <?php
+session_save_path('sess');
+session_start();
+require_once 'CAS.class.php';
+if (!isset($_SESSION['user']))
+{
+	$user = CAS::authenticate();
+	if ($user != -1) $_SESSION['user'] = $user;
+}
+else CAS::login();
+?>
+
            
 
 		<li class="divider-vertical"></li>
