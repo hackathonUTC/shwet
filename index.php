226ade01c200002e04fa06a889fedf1829482f0c
NicolasSzewe <NicolasSzewe@users.noreply.github.com>
2015-06-14 06:11:52 +0200

    modify url

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ else
 	{
 		$_SESSION['user'] = $user;
 		$_SESSION['ticket'] = $_GET['ticket'];
-		header('Location: /shwet.html');
+		header('Location: shwet/shwet.html');
 	}
 	else CAS::login();
 }
