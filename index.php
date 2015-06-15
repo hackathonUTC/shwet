178bebe77bb732a8a473d6d270d30da94ad3e64d
Mewen <mewenmichel@yahoo.fr>
2015-06-15 12:00:35 +0200

    Correction du CAS, c'était mieux avant ><'

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
-session_start();
-require_once 'CONF.class.php';
-require_once 'CAS.class.php';
-$user = CAS::authenticate();
-if ($user != -1)
-{
-	echo $user;
-	$_SESSION['user'] = $user;
-	$_SESSION['ticket'] = $_GET['ticket'];
-	// var_dump($_SESSION);
-	header('Location: shwet.php');
-}
-else CAS::login();
+include_once('verify_connection.php');
+// session_start();
+// require_once 'CONF.class.php';
+// require_once 'CAS.class.php';
+// $user = CAS::authenticate();
+// if ($user != -1)
+// {
+// 	echo $user;
+// 	$_SESSION['user'] = $user;
+// 	$_SESSION['ticket'] = $_GET['ticket'];
+// 	// var_dump($_SESSION);
+// 	header('Location: shwet.php');
+// }
+// else CAS::login();
--- verify_connection.php
+++ verify_connection.php
@@ -2,9 +2,20 @@
 session_start();
 require_once 'CONF.class.php';
 require_once 'CAS.class.php';
-$user = CAS::authenticate();
-if ($user == -1){
-	// echo "pas connecté";
-	// header('Location: shwet.php');
-	CAS::login();
+
+if ( !isset($_SESSION['user'])){
+	$user = CAS::authenticate();
+	if ($user != -1) {
+		// On revient du CAS et on a bien récupéré les infos :)
+
+		// echo "Connecté";
+		// echo $user;
+		$_SESSION['user'] = $user;
+		$_SESSION['ticket'] = $_GET['ticket'];
+		header('Location: shwet.php');
+	}
+	else {
+		// L'user n'est pas connecté et ne revient pas du CAS, donc on l'envoie là-bas
+		CAS::login();
+	}
 }
