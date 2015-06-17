e020ccab4e521f43f545aeccc73a1f874bb78b84
Mewen <mewenmichel@yahoo.fr>
2015-06-17 15:37:35 +0200

    L'upload marche :)

--- upload.php
+++ upload.php
@@ -11,25 +11,62 @@ function upload($index,$destination)
      return move_uploaded_file($_FILES[$index]['tmp_name'],$destination);
 }
 
-    echo "<br><br><br><br>";
-    var_dump($_POST);
-    echo "Is dir : ".(is_dir ( "./docs/FQ01/TD/" )?'oui':'non');
-    $uv = $strtoupper($_POST['u']);
-    $type = $_POST['selectType'];
-    $nom = $_POST['nomfichier'];
-    $note = $_POST['note'];
-    $semestre = $_POST['semestre'];
-    $commentaire = $_POST['commentaire'];
-    $filename = basename($_FILES['fichierUpload']['name']);
+function isUV($uv){
+  include('db_connect.php');
+
+  $req = "SELECT COUNT(*) FROM uvbranche WHERE uv='".mysql_escape_string($uv)."';";
+  $retour = db_query($req);
+  $row = mysql_fetch_array($retour);
+  if ( intval($row['COUNT(*)']) > 0){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isType($type){
+  // ========= /!\ attention : à refaire ! =======
+  if (($type == "TP") || ($type == "TD") || ($type == "Autres"))
+    return true;
+  else
+    return false;
+}
+
+  echo "<br><br><br><br>";
+  // var_dump($_POST);
+  // echo "Is dir : ".(is_dir ( "./docs/FQ01/TD/" )?'oui':'non');
+  $uv = strtoupper($_POST['u']);
+  $type = $_POST['selectType'];
+  $nom = $_POST['nomfichier'];
+  $note = $_POST['note'];
+  $semestre = $_POST['semestre'];
+  $commentaire = $_POST['commentaire'];
+  $filename = basename($_FILES['fichierUpload']['name']);
   $ext = pathinfo($filename, PATHINFO_EXTENSION);
-	
-  // if (isUV($uv) && !is_dir( "./docs/".$uv ))
-  //   mkdir("./docs/".$uv, '0740');
+  $path = "./docs/".$uv."/".$type."/";
 
-  // if (isType($type) && !is_dir( "./docs/".$uv."/".$type ))
-  //   mkdir("./docs/".$uv."/".$type, '0740');
+  // ========= /!\ attention : à refaire ! =======
+  $id = "cdvnejbomd";
 
-  	// $upload_fichier = upload($filename,"docs"+$uv+"/"+$type+"/"+$id+"/"+$ext);
+  if (isUV($uv) && isType($type) && !is_dir( $path )){
+    if (mkdir( $path , 0777)){
+      echo "Dossier ".$path." créé :) ";
+        // $upload_fichier = upload($filename, $path.$id.".".$ext );
+      if( move_uploaded_file( $_FILES["fichierUpload"]["tmp_name"], $path.$id.".".$ext ) ){
+        echo "Fichier déplacé \o/ ";
+      } else {
+        echo "Erreur au déplacement de " . $_FILES["fichierUpload"]["tmp_name"] ." à ". $path.$id.".".$ext."   ";
+      }
+    } else
+      echo "Erreur lors de la création d'un dossier :/ ";
 
-  // 
+  } else {
+      echo "Le dossier existe déjà ou bien ça n'est pas une uv ! ";
+      if( move_uploaded_file($_FILES["fichierUpload"]["tmp_name"], $path.$id.".".$ext ) ){
+        echo "Fichier déplacé \o/ ";
+      } else {
+        echo "Erreur au déplacement de " . $_FILES["fichierUpload"]["tmp_name"]." à ". $path.$id.".".$ext."   ";
+      }
+  }
+  // var_dump($_FILES);
 ?>
